0efa3dc3ed81c2f3bae5b77576fd8d46b72067c9
LKH <kangholee8512@gmail.com>
2023-05-15 06:56:04 +0900

    각 학생의 이름/ 출생년도 반영 - 출생년도는 String 가공 기능 재활용

--- app/src/main/java/com/cnc/listviewpractice/adapters/StudentAdapter.kt
+++ app/src/main/java/com/cnc/listviewpractice/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.cnc.listviewpractice.R
 import com.cnc.listviewpractice.datas.Student
 
@@ -22,6 +23,17 @@ class StudentAdapter(
         }
 
         val row = tempRow!!
+
+        val studentData = mList[position]
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val birthYearTxt = row.findViewById<TextView>(R.id.birthYearTxt)
+
+        nameTxt.text = studentData.name
+        birthYearTxt.text = "(${studentData.birthYear}년생)"
+
+
+
 
         return row
     }
