268223d864bcd4acb2c99f7ec42a54094cca0520
LKH <kangholee8512@gmail.com>
2023-05-14 22:58:41 +0900

    adapter 뼈대작업 완료

--- app/src/main/java/com/cnc/listviewpractice/adapters/StudentAdapter.kt
+++ app/src/main/java/com/cnc/listviewpractice/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.cnc.listviewpractice.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.cnc.listviewpractice.R
 import com.cnc.listviewpractice.datas.Student
 
 class StudentAdapter(
@@ -9,4 +13,16 @@ class StudentAdapter(
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
 
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow =  convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 }
