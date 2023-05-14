b1243e06df204ddf3fcb68a3bb060bc3d2eb0a33
LKH <kangholee8512@gmail.com>
2023-05-14 23:40:48 +0900

    adapter 뼈대작업 완료

--- app/src/main/java/com/cnc/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/cnc/listviewpractice/MainActivity.kt
@@ -2,12 +2,18 @@ package com.cnc.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cnc.listviewpractice.adapters.StudentAdapter
 import com.cnc.listviewpractice.databinding.ActivityMainBinding
+import com.cnc.listviewpractice.datas.Student
 
 class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
     // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
     private val binding get() = mBinding!!
+
+    val mStudentList = ArrayList<Student>()
+
+    lateinit var mAdapter : StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,16 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mStudentList.add(Student("강호", 1985))
+        mStudentList.add(Student("으네", 1993))
+        mStudentList.add(Student("정솔", 1992))
+        mStudentList.add(Student("영민", 1985))
+        mStudentList.add(Student("용준", 1985))
+        mStudentList.add(Student("진쥬", 1984))
+        mStudentList.add(Student("규태", 1985))
 
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        binding.studentListView.adapter = mAdapter
     }
 }
